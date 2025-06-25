4b1c80962c14954559bdbf0c1771affc1031fcf6
Janakiraman <55402772+Rams1303@users.noreply.github.com>
2025-06-25 14:58:02 +0530

    Update VideoCompressor.java

--- src/android/VideoCompressor.java
+++ src/android/VideoCompressor.java
@@ -1,1 +1,21 @@
+package com.example.videocompressor;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class VideoCompressor extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("compress")) {
+            String inputPath = args.getString(0);
+            String outputName = args.getString(1);
+
+            // Simulate success
+            callbackContext.success("compressed_" + outputName);
+            return true;
+        }
+        return false;
+    }
+}
 
