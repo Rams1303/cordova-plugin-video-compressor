24c84534f1100a8e2eb6afd2ce486f25f1d1e276
Janakiraman <55402772+Rams1303@users.noreply.github.com>
2025-06-25 14:57:19 +0530

    Update VideoCompressor.js

--- www/VideoCompressor.js
+++ www/VideoCompressor.js
@@ -1,1 +1,6 @@
+var exec = require('cordova/exec');
+
+exports.compress = function (inputPath, outputFileName, success, error) {
+    exec(success, error, 'VideoCompressor', 'compress', [inputPath, outputFileName]);
+};
 
