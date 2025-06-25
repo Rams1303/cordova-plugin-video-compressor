eb6d8d9b9161cfb9f5c70836196ee2fdc20637f7
Janakiraman <55402772+Rams1303@users.noreply.github.com>
2025-06-25 15:01:03 +0530

    Update VideoCompressor.swift

--- src/ios/VideoCompressor.swift
+++ src/ios/VideoCompressor.swift
@@ -1,1 +1,16 @@
+import Foundation
+import AVFoundation
+
+@objc(VideoCompressor)
+class VideoCompressor: CDVPlugin {
+
+    @objc(compress:)
+    func compress(command: CDVInvokedUrlCommand) {
+        let inputPath = command.arguments[0] as! String
+        let outputName = command.arguments[1] as! String
+
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "compressed_" + outputName)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
+}
 
